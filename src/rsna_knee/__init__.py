4b85418eb983520ec03947256474a4aa61d85cdd
Mohammed Talafha <126190678+mtalafha90@users.noreply.github.com>
2026-08-07 15:52:14 +0400

    Add package initializer

---
+++ src/rsna_knee/__init__.py
@@ -0,0 +1,6 @@
+"""RSNA 2026 Knee MRI abnormality detection toolkit."""
+
+from .constants import N_TARGETS, SUBMISSION_COLUMNS, TARGETS, TARGET_SLUGS
+
+__all__ = ["TARGETS", "TARGET_SLUGS", "SUBMISSION_COLUMNS", "N_TARGETS"]
+__version__ = "0.1.0"
